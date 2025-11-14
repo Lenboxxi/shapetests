206df86df196e3b22198c9ab7bcd24020d452f7b
Tim Kleinhans <tim@tkl-tech.de>
2025-11-14 10:21:05 +0100

    Added parameterized tests for parallelogram

--- src/test/kotlin/ShapeDetectionTest.kt
+++ src/test/kotlin/ShapeDetectionTest.kt
@@ -1,8 +1,15 @@
 import com.example.Shape
 import com.example.detectShape
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ShapeDetectionTest {
     @Test
     fun `detects rectangle`() {
@@ -12,8 +19,9 @@ class ShapeDetectionTest {
         )
     }
 
-    @Test
-    fun `detects parallelogram`() {
+    @ParameterizedTest
+    @MethodSource("generateParallelogramAngles")
+    fun `detects parallelogram`(a: Int, b: Int, c: Int, d: Int) {
         assertEquals(
             Shape.PARALLELOGRAM,
             detectShape(110, 70, 110, 70)
@@ -22,6 +30,11 @@ class ShapeDetectionTest {
         assertEquals(
             Shape.PARALLELOGRAM,
             detectShape(80, 100, 80, 100)
+        )
+
+        assertEquals(
+            Shape.PARALLELOGRAM,
+            detectShape(a, b, c, d)
         )
     }
 
@@ -53,55 +66,52 @@ class ShapeDetectionTest {
         )
     }
 
-    @Test
-    fun `invalid when any angle is zero or negative`() {
+    @ParameterizedTest
+    @CsvSource(
+        "0,   90,  90, 180",
+        "90,   0,  90, 180",
+        "90,  90,   0, 180",
+        "90,  90, 180,   0",
+        "-10, 90, 150, 130",
+        "90, -10, 150, 130",
+        "150, 90, -10, 130",
+        "130, 90, 150, -10"
+    )
+    fun `invalid when any angle is zero or negative`(a: Int, b: Int, c: Int, d: Int) {
         assertEquals(
             Shape.INVALID,
-            detectShape(0, 90, 90, 180)
-        )
-        assertEquals(
-            Shape.INVALID,
-            detectShape(90, 0, 90, 180)
-        )
-        assertEquals(
-            Shape.INVALID,
-            detectShape(90, 90, 0, 180)
-        )
-        assertEquals(
-            Shape.INVALID,
-            detectShape(90, 90, 180, 0)
-        )
-        assertEquals(
-            Shape.INVALID,
-            detectShape(-10, 90, 150, 130)
-        )
-        assertEquals(
-            Shape.INVALID,
-            detectShape(90, -10, 150, 130)
-        )
-        assertEquals(
-            Shape.INVALID,
-            detectShape(150, 90, -10, 130)
-        )
-        assertEquals(
-            Shape.INVALID,
-            detectShape(130, 90, 150, -10)
+            detectShape(a, b, c, d)
         )
     }
 
-    @Test
-    fun `invalid parallelogram candidates`() {
+    @ParameterizedTest
+    @CsvSource(
+        "110, 70, 110, 60",
+        "100, 70, 110, 70",
+        "90, 45, 90, 45"
+    )
+    fun `invalid parallelogram candidates`(a: Int, b: Int, c: Int, d: Int) {
         assertEquals(
             Shape.INVALID,
-            detectShape(110, 70, 110, 60) // b != d
+            detectShape(a, b, c, d)
         )
-        assertEquals(
-            Shape.INVALID,
-            detectShape(100, 70, 110, 70) // c != a
-        )
-        assertEquals(
-            Shape.INVALID,
-            detectShape(90, 45, 90, 45) // sum != 360
-        )
+    }
+
+
+    fun generateParallelogramAngles(): Stream<Arguments> {
+        val args = mutableListOf<Arguments>()
+
+        for (a in 1..179) {
+            val b = 180 - a
+
+            if (a == b) continue
+
+            val c = a
+            val d = b
+
+            args += Arguments.of(a, b, c, d)
+        }
+
+        return args.stream()
     }
 }
